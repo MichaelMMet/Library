38f96b4eaba3402a6da8795d936d31274483453f
Michael Metcalf <mikegmac14@gmail.com>
2022-10-16 23:18:03 -0700

    Test

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const newBook = document.querySelector(".new-book");
 const title = document.querySelector("#title");
-newBook.addEventListener("click", addBookToLirary)
-let myLibrary = [];
+// newBook.addEventListener("click", addBookToLirary)
+const addBook = document.querySelector(".add-book-button");
+addBook.addEventListener("click", displayTheForm);
+document.getElementById("add-book-form")
+
 
 
 // book cunstructor
@@ -23,12 +26,41 @@ this.info = function() {
 function addBookToLirary(Title, Author, Pages, Read){
 let book = new Book(Title, Author, Pages, Read);
 myLibrary.push(book);
-
-console.log(title.innerHTML);
+displayBooks();
 }
+
+const submitBook = document.querySelector(".submit-button");
+submitBook.addEventListener("click", intakeFormData);
+
+
+function intakeFormData(){
+  let Title = document.getElementById("Title").value;
+  let Author =document.getElementById("Author").value;
+  let Pages = document.getElementById("Pages").value;
+  let Read = document.getElementById("Read").value;
+
+  if ((title == "") || (Author == "") || (Pages == "") || (Read ="")){
+    return;
+  }
+
+  function addBookToLirary(Title, Author, Pages, Read);
+
+  document.getElementById("add-book").reset();
+}
+ 
+function displayTheForm(){
+  document.getElementById("new-book-form").style.display = "";
+}
+
 
 function displayBooks(){
   const bookContainer = document.querySelector(".book-container");
+
+const removeDivs = document.querySelectorAll(".book");
+console.log("show how many", removeDivs);
+for(let i = 0; i < removeDivs.length; i++){
+removeDivs[i].remove();
+}
 
 myLibrary.forEach(myLibrary => {
 
